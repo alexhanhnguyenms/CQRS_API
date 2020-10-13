8770a35e6c01d58088bec667e955f93bb126495c
alexhanhnguyenms@gmail.com <alexhanhnguyenms@gmail.com>
2020-10-13 23:01:43 +0700

    init

---
+++ CQRS_API/Context/IApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using CQRS_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CQRS_API.Context
+{
+    public interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ CQRS_API/Features/ProductFeatures/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,33 @@
+﻿using CQRS_API.Context;
+using CQRS_API.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRS_API.Features.ProductFeatures.Queries
+{
+    public class GetAllProductsQuery: IRequest<IEnumerable<Product>>
+    {
+        public class GetAllProductsQueryHandler: IRequestHandler<GetAllProductsQuery, IEnumerable<Product>>
+        {
+            private readonly IApplicationContext _context;
+            public GetAllProductsQueryHandler(IApplicationContext context)
+            {
+                _context = context;
+            }
+            public async Task<IEnumerable<Product>> Handle(GetAllProductsQuery query, CancellationToken cancellationToken)
+            {
+                var productList = await _context.Products.ToListAsync();
+                if (productList == null)
+                {
+                    return null;
+                }
+                return productList.AsReadOnly();
+            }
+        }
+    }
+}
